0ccdcee7025285e1f1b0c440093e10be4c4610be
EdricYeo117 <edricyeo.jr@gmail.com>
2024-06-26 23:28:25 +0800

    Update valdiate user logic

--- middleware/validateUser.js
+++ middleware/validateUser.js
@@ -1,21 +1,34 @@
 const Joi = require("joi");
 
 const validateUser = (req, res, next) => {
+  // Reg Expression for Password
+  const passwordPattern = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$"
+  );
+
   const schema = Joi.object({
     username: Joi.string().min(3).max(50).required(),
-    password: Joi.string().min(6).required(),
-    firstname: Joi.string().allow('', null).max(50),
-    lastname: Joi.string().allow('', null).max(50),
+    // Validate password with regex pattern
+    password: Joi.string().pattern(passwordPattern)
+      .messages({
+        "string.pattern.base": "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+      })
+      .required(),
+    firstname: Joi.string().allow("", null).max(50),
+    lastname: Joi.string().allow("", null).max(50),
+    // Validate roles using joi.object, as roles is an object with keys User, Volunteer, and Admin
     roles: Joi.object({
-        User: Joi.number().required()
-      }).default({ User: 2001 }),
-    refreshToken: Joi.string().allow('', null),
-    address: Joi.string().allow('', null),
+      User: Joi.number().valid(2001),
+      Volunteer: Joi.number().valid(2002),
+      Admin: Joi.number().valid(2003),
+    }).unknown(false).required(), // Unknown(false) because only have these 3 roles, no additional keys allowed
+    refreshToken: Joi.string().allow("", null),
+    address: Joi.string().allow("", null),
     dietaryRestrictions: Joi.array().items(Joi.string()),
     intolerances: Joi.array().items(Joi.string()),
     excludedIngredients: Joi.array().items(Joi.string()),
     email: Joi.string().email().required(),
-    contact: Joi.string().required()
+    contact: Joi.string().required(),
   });
 
   const validation = schema.validate(req.body, { abortEarly: false });
