c2c3b6debee4aa1a97c27ab5d426470137eb2c88
EdricYeo117 <edricyeo.jr@gmail.com>
2024-07-07 20:52:11 +0800

    Added comments to the middleware files

--- middleware/validateRecipe.js
+++ middleware/validateRecipe.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 
+// Define Joi schema for recipe validation
 const validateRecipeSchema = Joi.object({
   title: Joi.string().optional(),
   imageurl: Joi.string().uri().optional(),
@@ -8,14 +9,17 @@ const validateRecipeSchema = Joi.object({
   pricePerServing: Joi.number().optional()
 }).options({ abortEarly: false });
 
+// Middleware to validate recipe data
 const validateRecipe = (req, res, next) => {
+  // Validate req.body against the schema
   const { error } = validateRecipeSchema.validate(req.body);
 
   if (error) {
+    // Extract error details from Joi validation error
     const errorDetails = error.details.map(detail => detail.message);
     return res.status(400).json({ message: 'Validation errors', errors: errorDetails });
   }
-
+  // Proceed to the next middleware or route handler
   next();
 };
 
