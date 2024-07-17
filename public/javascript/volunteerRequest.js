28d5e4602f6afcc2132d69b2390d802adde4b34d
Koyonari <yongshyan.an@gmail.com>
2024-07-18 02:53:28 +0800

    chore: replace paths

--- public/javascript/volunteerRequest.js
+++ public/javascript/volunteerRequest.js
@@ -1,3 +1,10 @@
+// Select the list element where requests will be displayed
+let list = document.querySelector(".list");
+let listvol = document.querySelector(".list-vol");
+
 // Fetch user id and token
 const userId = localStorage.getItem('UserId');
 const accessToken = localStorage.getItem('AccessToken');
+
+// Global variable to store the requests array
+let globalRequests = [];
